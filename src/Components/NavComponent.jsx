4005b41d940cd8d1787950d8ea698cc903a0e1bf
JAldo1980 <jamesalderman1980@gmail.com>
2023-11-28 15:28:49 +0000

    added nav section

--- src/Components/NavComponent.jsx
+++ src/Components/NavComponent.jsx
@@ -6,11 +6,13 @@ function NavComponent() {
     <>
       <div className="nav-component">
         <div className="primary-menu">
-          <img
-            src="/images/david-alderman-logo-removebg-preview.png"
-            alt="david alderman logo"
-            className="david-alderman-logo"
-          />
+          <Link to="/">
+            <img
+              src="/images/david-alderman-logo-removebg-preview.png"
+              alt="david alderman logo"
+              className="david-alderman-logo"
+            />
+          </Link>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/contact">Contact</Link>
@@ -23,6 +25,38 @@ function NavComponent() {
             className="cart-img"
           />
           <img src="/images/user.png" alt="user-img" className="user-img" />
+        </div>
+        <div className="mobile-nav-flex">
+          <Link to="/">
+            <img
+              src="/images/david-alderman-logo-removebg-preview.png"
+              alt="david alderman logo"
+              className="david-alderman-logo-mob"
+            />
+          </Link>
+          <div className="hamburger">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+        </div>
+
+        <div className="mobile-nav-container">
+          <div className="mobile-nav">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <img
+              src="/images/shopping-cart-light.png"
+              alt="cart-image"
+              className="cart-img-mob"
+            />
+            <img
+              src="/images/user.png"
+              alt="user-img"
+              className="user-img-mob"
+            />
+          </div>
         </div>
       </div>
     </>
